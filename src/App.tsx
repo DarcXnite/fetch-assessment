65547ddf650e2e6d2db725fb00bd2b68eb1174ec
Davis Nguyen <davisnguyen@Daviss-MacBook-Air.local>
2022-10-24 16:43:20 -0500

    custom hook and initial form complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import useAxios from './hooks/useAxios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface UserDetails {
+  name: string
+  email: string
+  password: string
+  occupation: string
+  state: string
 }
 
-export default App;
+interface States {
+  name: string
+  abbreviations: string
+}
+
+const initialForm = {
+  name: '',
+  email: '',
+  password: '',
+  occupation: '',
+  state: '',
+}
+
+function App() {
+  const { data, status } = useAxios(
+    'https://frontend-take-home.fetchrewards.com/form'
+  )
+  const [details, setDetails] = useState<UserDetails>(initialForm)
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(details)
+  }
+
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setDetails(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }))
+    console.log(details)
+  }
+
+  return (
+    <>
+      <header>Status Code: {status}</header>
+      <div className='input-form'>
+        <form action='submit' onSubmit={e => onSubmit}>
+          <input
+            type='text'
+            placeholder='Name'
+            value={details?.name}
+            name='name'
+            onChange={handleChange}
+          />
+          <input
+            type='email'
+            placeholder='Email'
+            value={details?.email}
+            name='email'
+            onChange={handleChange}
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            value={details?.password}
+            name='password'
+            onChange={handleChange}
+          />
+          <label htmlFor='occupations'>Select an Occupation:</label>
+          <select
+            name='occupation'
+            id='occupations'
+            value={details?.occupation}
+            onChange={handleChange}
+          >
+            {data?.occupations.map((occ: string, idx: number) => (
+              <option key={idx} value={occ}>
+                {occ}
+              </option>
+            ))}
+          </select>
+          <label htmlFor='states'>Select a State:</label>
+          <select
+            name='state'
+            id='states'
+            value={details?.state}
+            onChange={handleChange}
+          >
+            {data?.states.map((state: States, idx: number) => {
+              const { name } = state
+              return (
+                <option key={idx} value={name}>
+                  {name}
+                </option>
+              )
+            })}
+          </select>
+          <button>Submit</button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/hooks/useAxios.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+interface dataParams {
+  occupations: any[]
+  states: any[]
+}
+
+export default function useAxios(url: string) {
+  const [data, setData] = useState<dataParams>()
+  const [status, setStatus] = useState<number>()
+  useEffect(() => {
+    const controller = new AbortController()
+    const fetchData = async (url: string) => {
+      try {
+        const res = await axios.get(url)
+        setData(res.data)
+        setStatus(res.status)
+      } catch (err) {
+        console.warn(err)
+      }
+    }
+
+    fetchData(url)
+    return () => controller.abort()
+  }, [url])
+
+  return { data, status }
+}
